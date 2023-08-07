f5bef6a0f60a9e77f7e7152bcfa70e9c30184ad6
BanuPriya93 <129736978+BanuPriya93@users.noreply.github.com>
2023-08-07 22:57:58 +0530

    Create newadd.java

---
+++ selni/src/selni/newadd.java
@@ -0,0 +1,16 @@
+package com.demo.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+
+	@Test
+	public void testCreateCustomerModule() {
+		Reporter.log("CreateCustomerModule",true);
+	}
+	
+}
+		
+	
+
