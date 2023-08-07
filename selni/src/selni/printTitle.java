7a9a7bfc1b2db0951f433469453cddace698e3cd
MONISHA K E <MONISHA K E@LAPTOP-QLF97QS2>
2023-08-07 22:34:48 +0530

    nw

---
+++ selni/src/selni/printTitle.java
@@ -0,0 +1,19 @@
+package selni;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class printTitle {
+
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		String Title = driver.getTitle();
+System.out.println(Title);
+driver.close();
+	}
+
+}
